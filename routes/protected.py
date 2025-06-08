4227a1b2d35c5678b8b7ac8760fba76aba9db09f
nnNyx <monkeynik04@gmail.com>
2025-06-08 13:04:02 +0200

    Dodavanje `/whoami` rute koja vraca korisnicke podatke

--- routes/protected.py
+++ routes/protected.py
@@ -1,5 +1,7 @@
 from flask import Blueprint, jsonify
 from utils.auth_utils import token_required
+from models.user import User
+from models.BusinessUser import BusinessUser
 
 protected_bp = Blueprint('protected', __name__)
 
@@ -7,3 +9,27 @@ protected_bp = Blueprint('protected', __name__)
 @token_required
 def protected(current_user):
     return jsonify({"message": f"Hello world {current_user.username}"})
+
+@protected_bp.route("/whoami", methods=["GET"])
+@token_required
+def whoami(current_user):
+    # Probaj dohvatiti podatke o (business) korisniku
+    if isinstance(current_user, BusinessUser):
+        response = {
+            "name": current_user.name,
+            "email": current_user.email,
+            "phone": current_user.phone,
+            "address": current_user.address,
+            "parkingTotal": current_user.parkingtotal,
+            "opis": current_user.opis,
+            "type": "business"
+        }
+    elif isinstance(current_user, User):
+        response = {
+            "username": current_user.username,
+            "balance": current_user.balance,
+            "type": "user"
+        }
+    else:
+        response = {"message": "Unknown user type"}
+    return jsonify(response)
